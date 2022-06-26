d9a94e6ed69cb66da59452a168cff7fdc2cb59e4
Brandon Xie <17865192193@163.com>
2022-06-26 23:36:14 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,13 +1,12 @@
 ### ---------------------------training-------------------------------
 # mask warp 
-CUDA_VISIBLE_DEVICES=0,1 nohup python run.py --config config/Mixed_data-10-8-wMaskWarp.yaml \
-1>Mixed_data-10-8-wMaskWarp.out 2>Mixed_data-10-8-wMaskWarp.err &
+CUDA_VISIBLE_DEVICES=0,1,2,3 python run.py --config config/Mixed_data-10-8-wMaskWarp.yaml 
 
 ### ---------------------------generation-------------------------------
 ## mask warp
 # relative nobest
 CUDA_VISIBLE_DEVICES=0 python demo.py --config config/Mixed_data-10-8-wMaskWarp-aug.yaml  \
-    --checkpoint 'log/Mixed_data-10-8-wMaskWarp-aug 25_06_22_16.11.40/00000049-checkpoint.pth.tar' \
+    --checkpoint 'path to the checkpoints' \
     --result_video './ckpt/relative-nobest/wMaskWarp' \
     --mode 'relative'
 
