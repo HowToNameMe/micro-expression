d143f106e42425562744e462c9b3d38890a3bb34
Brandon Xie <17865192193@163.com>
2022-06-29 17:02:50 +0800

    Update run.py

--- run.py
+++ run.py
@@ -16,7 +16,6 @@ from modules.dense_motion import DenseMotionNetwork
 from modules.avd_network import AVDNetwork
 import torch
 from train import train
-from train_avd import train_avd
 from reconstruction import reconstruction
 from animate import animate
 import os 
